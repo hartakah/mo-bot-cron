de214c7a03a5634ece1cb2a6d0a75b3189959900
hartakah <hartakah@gmail.com>
2018-11-18 21:40:05 +0900

    mod src path

--- src/make_price_list.sh
+++ src/make_price_list.sh
@@ -1,10 +1,10 @@
 #/usr/bin/bash
 
 # get price list from mtg goldfish
-python3 /home/Ophidian/work/make_price_list/get_price_list.py
+python3 /home/mo-bot/src/get_price_list.py
 
 # make personal price list
-/usr/bin/php /home/Ophidian/work/make_price_list/make_price_list.php
+/usr/bin/php /home/mo-bot/src/make_price_list.php
 
 # move personal price list to update
 cp /home/mo-bot/work/my_price_list.txt '/cygdrive/c/Program Files (x86)/MTGOLibrary/MTGO Library Bot/prices/PersonalPrices.txt'
